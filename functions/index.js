86569b43506b0b9ef03805937b8238893e861d81
vabira200 <Vaishnavi_AbiramiWM@comcast.com>
2020-10-10 17:12:52 +0530

    adding route

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,12 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+
+app.get("/time", function (req, res) {
+    res.status(200).send(`${Date.now()}`);
+})
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
-});
+exports.app = functions.https.onRequest(app);
